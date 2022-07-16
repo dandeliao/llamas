69ae339231fc445e8732467099d9f1f954e8b896
dandeliao <danielramos@tuta.io>
2022-07-16 18:36:23 -0300

    treats simulation data before sending

--- llamas-api/src/index.js
+++ llamas-api/src/index.js
@@ -14,6 +14,20 @@ app.use(express.urlencoded({extended: true}));
 // ---
 // simulation
 
+function unbrainLlamas(llamas) {
+	
+	let zombieLlamas = [];
+
+	for(let i = 0; i < llamas.length; i++) {
+		let llama = _.cloneDeep(llamas[i]);
+		delete llama.brain;
+		zombieLlamas.push(llama);
+	}
+
+	return zombieLlamas;
+
+}
+
 function runSimulation(initialGround, initialLlamas, rounds) {
 	
 	console.log('Simulation has started.');
@@ -27,12 +41,19 @@ function runSimulation(initialGround, initialLlamas, rounds) {
 	for (let r = 0; r < rounds; r++) {
 		console.log('--- round', r);
 		newWorld = oneRound(ground, llamas);
-		timeline.push(_.cloneDeep(newWorld));
 		ground = _.cloneDeep(newWorld.ground);
 		llamas = _.cloneDeep(newWorld.llamas);
+
+		newWorld.llamas = unbrainLlamas(newWorld.llamas);
+		//timeline.push(_.cloneDeep(newWorld));
+		timeline.push(newWorld);
+
 	}
 	
 	console.log('Simulation has finished.');
+	console.log('a survivor:', llamas[llamas.length - 1]);
+	console.log('other survivor:', llamas[0]);
+
 	return timeline;
 }
 
@@ -45,7 +66,7 @@ app.get('/', (req, res) => {
 
 app.post('/run', (req, res) => {
 
-	const oldWorld = {'ground': randomGround(40, 40), 'llamas': randomLlamas(40, 40)};
+	const oldWorld = {'ground': randomGround(20, 30), 'llamas': randomLlamas(20, 30)};
 	const timeline = runSimulation(oldWorld.ground, oldWorld.llamas, req.body.steps);
 	res.json(timeline);
 });
